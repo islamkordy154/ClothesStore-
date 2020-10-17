3fa78cff4e9b275a4b13ed6fa236abe611b09025
islamkordy <islam.kordy@yahoo.com>
2020-10-17 08:17:43 +0200

    i just commit the proj

---
+++ ClothesStore/Components/NavigationMenueViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using ClothesStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClothesStore.Components
+{
+    public class NavigationMenueViewComponent : ViewComponent
+    {
+        private IProductRepository repository;
+
+        public NavigationMenueViewComponent(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedCategory = RouteData?.Values["category"];
+            return View(repository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x));
+        }
+
+    }
+}
